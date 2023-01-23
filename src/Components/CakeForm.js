e625ecf2f4a8fc380fb57868cd98aa6e962e0361
Salma Mounes <salma@Salmas-MacBook-Pro.local>
2023-01-23 16:35:11 +0000

    added states

--- src/Components/CakeForm.js
+++ src/Components/CakeForm.js
@@ -1,14 +1,31 @@
 import { useState } from 'react';
 
-import {}
-
-const CakeForm = () => {
 
 
+const CakeForm = ({addNewCake}) => {
+
+    const [cakeName, setCakeName] = useState("");
+    const [ingredients, setIngredients] = useState("");
+    const [rating,setRating] = useState("");
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const newCake = {
+        cakeName: cakeName,
+        ingredients:ingredients,
+        rating:rating,
+}
+addNewCake(newCake);
+setCakeName("")
+setIngredients("")
+setRating("")
+}
 
 
 
-    return (
+
+
+return (
         <div>
             <h2>Add Cake</h2>
     
@@ -26,7 +43,7 @@ const CakeForm = () => {
                    id="ingredients"
                    placeholder="Ingredients"
                    onChange={(event) => setIngredients(event.target.value)}
-                   value = {cakeName}/>
+                   value = {ingredients}/>
             <textarea cols="30" rows="1" id="ingredients"></textarea>
 
 
@@ -35,13 +52,13 @@ const CakeForm = () => {
                    id="rating" min={1} max={5}
                    placeholder="Rating"
                    onChange={(event) => setRating(event.target.value)}
-                   value = {cakeName}/>
+                   value = {rating}/>
 
             <input type="submit" value = "OK"/>
         </form>
         </div>
     )
+};
 
-}
 
 export default CakeForm;
